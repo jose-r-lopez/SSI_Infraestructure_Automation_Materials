92494e086664c86b10ab0dddef1b4c8e0e92c18c
José Manuel Redondo López <redondojose@uniovi.es>
2021-11-03 17:48:00 +0100

    Added AlmaLinux 8 files
    Added AlmaLinux 8 files

---
+++ Vagrantfiles/AlmaLinux_GUI/buildvm_vbox.bat
@@ -0,0 +1,20 @@
+set VAGRANT_EXPERIMENTAL="disks"
+vagrant up
+
+vagrant ssh -c "sudo dnf -y install gcc make perl kernel-devel kernel-headers bzip2 dkms"
+vagrant ssh -c "sudo dnf update kernel-*"
+
+vagrant halt
+vagrant up
+
+vagrant ssh -c "cd /opt/"
+vagrant ssh -c "sudo wget http://download.virtualbox.org/virtualbox/LATEST.TXT"
+vagrant ssh -c "sudo wget -c http://download.virtualbox.org/virtualbox/$(cat LATEST.TXT)/VBoxGuestAdditions_$(cat LATEST.TXT).iso -O /opt/VBGAdd.iso"
+vagrant ssh -c "sudo mount /opt/VBGAdd.iso -o loop /media"
+vagrant ssh -c "sudo sh /media/VBoxLinuxAdditions.run --nox11"
+vagrant ssh -c "sudo umount /media"
+vagrant ssh -c "sudo rm /opt/VBGAdd.iso"
+
+vagrant halt
+vagrant up
+
---
+++ Vagrantfiles/AlmaLinux_cmd/buildvm.bat
@@ -0,0 +1,10 @@
+set VAGRANT_EXPERIMENTAL="disks"
+vagrant up
+
+vagrant ssh -c "sudo dnf -y install epel-release"
+vagrant ssh -c "sudo dnf -y install gcc make perl kernel-devel kernel-headers bzip2 dkms"
+vagrant ssh -c "sudo dnf update kernel-*"
+
+vagrant halt
+vagrant up
+
