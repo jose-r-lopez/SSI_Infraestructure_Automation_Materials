8f1e600132287c6e0724b051bf26f56da9b391ee
José Manuel Redondo López <redondojose@uniovi.es>
2021-11-16 18:41:31 +0100

    Update vagrant_user_setup.sh

--- Vagrantfiles/customization_scripts/redhat_customization/vagrant_user_setup.sh
+++ Vagrantfiles/customization_scripts/redhat_customization/vagrant_user_setup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 echo "Configuring Vagrant user"
-sudo cp /home/vagrant/custom_files/.nanorc /root
-sudo cp /home/vagrant/custom_files/.nanorc /home/vagrant
+sudo cp /home/vagrant/custom_files/nanorc /root/.nanorc
+sudo cp /home/vagrant/custom_files/nanorc /home/vagrant/.nanorc
 sudo cp /home/vagrant/custom_files/tmux.conf /etc/
 sudo chmod o+r /etc/tmux.conf
 
